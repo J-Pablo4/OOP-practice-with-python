bf66b7fc28cc9ca3b846dc79fbb2651d6161a086
Juan Pablo <pablo.lopez@iteso.mx>
2023-01-06 14:57:37 -0600

    Created a child class

--- main.py
+++ main.py
@@ -48,4 +48,26 @@ class Item:
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-phone1 = Item(jscPhone)
+
+class Phone(Item):
+    all = []
+
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        # Call to super function to have access to all attributes / methods
+        super().__init__(
+            name, price, quantity
+        )
+
+        # Run validations to the received arguments
+        assert broken_phones >= 0, f"Broken Phones {broken_phones} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.broken_phones = broken_phones
+
+        # Actions to execute
+        Phone.all.append(self)
+
+
+phone1 = Phone("jscPhonev10", 500, 5, 1)
+
+
